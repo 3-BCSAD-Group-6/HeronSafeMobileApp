bd198431e51182f7ee5a3117ae4a3a9ba2a04cd8
jamae10 <52740452+jamae10@users.noreply.github.com>
2021-11-03 22:46:58 +0800

    login/register ui

--- app/src/main/java/com/example/heronsafeapp/LoginActivity.java
+++ app/src/main/java/com/example/heronsafeapp/LoginActivity.java
@@ -2,13 +2,27 @@ package com.example.heronsafeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class LoginActivity extends AppCompatActivity {
+    ImageButton btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnBack = findViewById(R.id.btnBackLogin);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent b = new Intent(LoginActivity.this, StartUp.class);
+                startActivity(b);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/heronsafeapp/StartUp.java
+++ app/src/main/java/com/example/heronsafeapp/StartUp.java
@@ -2,13 +2,28 @@ package com.example.heronsafeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartUp extends AppCompatActivity {
-
+Button btnLogin, btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_up);
+
+        btnLogin = findViewById(R.id.btnLoginStartUp);
+        btnRegister = findViewById(R.id.btnRegisterStartUp);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent l = new Intent(StartUp.this,LoginActivity.class);
+                startActivity(l);
+            }
+        });
+
     }
 }
