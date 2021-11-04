280d8efcf2ea3b3ade06c19cb0310ce6b6032ebe
jamae10 <52740452+jamae10@users.noreply.github.com>
2021-11-04 13:25:24 +0800

    Login - working

--- app/src/main/java/com/example/heronsafeapp/LoginActivity.java
+++ app/src/main/java/com/example/heronsafeapp/LoginActivity.java
@@ -4,11 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class LoginActivity extends AppCompatActivity {
 ImageButton btnBack;
+TextInputEditText etEmail, etPassword;
+Button btnLogin;
+TextView tvSignup;
+ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,12 +30,77 @@ ImageButton btnBack;
 
         btnBack = findViewById(R.id.btnBackLogin);
 
+        etEmail = findViewById(R.id.etLoginEmail);
+        etPassword = findViewById(R.id.etLoginPassword);
+
+        btnLogin = findViewById(R.id.btnLogin);
+
+        tvSignup = findViewById(R.id.tvRegisterNow);
+
+        progressBar = findViewById(R.id.progressBarLogin);
+
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent b = new Intent(LoginActivity.this, StartUp.class);
                 startActivity(b);
                 finish();
+            }
+        });
+
+        tvSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegistrationActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, password;
+
+                email = String.valueOf(etEmail.getText());
+                password = String.valueOf(etPassword.getText());
+
+                if (!email.equals("") && !password.equals("")){
+
+                    progressBar.setVisibility(View.VISIBLE);
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            String[] field = new String[2];
+                            field[0] = "email";
+                            field[1] = "password";
+                            //Creating array for data
+                            String[] data = new String[2];
+                            data[0] = email;
+                            data[1] = password;
+                            PutData putData = new PutData("http://192.168.100.165/heron-safe/login.php", "POST", field, data);
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    progressBar.setVisibility(View.GONE);
+                                    String result = putData.getResult();
+                                    if(result.equals("Login Success")){
+                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+                                        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                    else{
+                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            }
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "All Fields are Required", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
