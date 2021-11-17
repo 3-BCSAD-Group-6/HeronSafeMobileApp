eb041b739437bf9fc825ae6ae38e45d544945cb2
jamae10 <52740452+jamae10@users.noreply.github.com>
2021-11-17 23:39:27 +0800

    add activities

---
+++ app/src/main/java/com/example/heronsafeapp/HealthScreeningActivity.java
@@ -0,0 +1,14 @@
+package com.example.heronsafeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HealthScreeningActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health_screening);
+    }
+}
