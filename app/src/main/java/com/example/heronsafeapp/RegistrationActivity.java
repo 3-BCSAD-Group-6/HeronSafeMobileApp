3a4c98533f94ead6377d47f0073ce80c38c9a967
jamae10 <52740452+jamae10@users.noreply.github.com>
2021-12-07 01:07:18 +0800

    registration update - w/ vaccine info

--- app/src/main/java/com/example/heronsafeapp/RegistrationActivity.java
+++ app/src/main/java/com/example/heronsafeapp/RegistrationActivity.java
@@ -8,7 +8,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.text.InputType;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
@@ -20,7 +24,10 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 import org.json.JSONException;
@@ -34,10 +41,16 @@ import java.util.Map;
 public class RegistrationActivity extends AppCompatActivity {
 private ProgressDialog progressDialog;
 ImageButton btnBack2;
-TextInputEditText etFullname, etStudentID, etEmail, etPassword, etContactNum;
+TextInputLayout tlVaccinetype, tl1stDose, tl2ndDose;
+TextInputEditText etFullname, etStudentID, etEmail, etPassword, etContactNum, etFirstVaccine, etSecondVaccine;
 Button btnRegister;
 String time = "";
+String [] ifVaccined = {"Yes", "No"};
+String [] VaccineType = {"Pfizer", "Moderna", "Sinovac", "AstraZeneca", "J&J/Janssen", "Sinopharm"};
 TextView tvSignin;
+AutoCompleteTextView ACVaccined, ACVaccineType;
+ArrayAdapter<String> ifVaccinedAdapter;
+ArrayAdapter<String> VaccineTypeAdapter;
 //ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +64,13 @@ TextView tvSignin;
         etEmail = findViewById(R.id.etRegisterEmail);
         etPassword = findViewById(R.id.etRegisterPassword);
         etContactNum = findViewById(R.id.etRegisterContactNum);
+        etFirstVaccine = findViewById(R.id.etFirstVaccineDate);
+        etSecondVaccine = findViewById(R.id.etSecondVaccineDate);
+
+        tlVaccinetype = findViewById(R.id.txtLayoutVaccineType);
+        tl1stDose = findViewById(R.id.txtLayoutFirstVaccine);
+        tl2ndDose = findViewById(R.id.txtLayoutSecondVaccine);
+
 
         btnRegister = findViewById(R.id.btnRegister);
 
@@ -58,6 +78,76 @@ TextView tvSignin;
 
         //progressBar = findViewById(R.id.progressBarRegister);
         progressDialog = new ProgressDialog(this);
+
+        etFirstVaccine.setInputType(InputType.TYPE_NULL);
+        etSecondVaccine.setInputType(InputType.TYPE_NULL);
+
+        ACVaccined = findViewById(R.id.tvACVaccined);
+        ACVaccineType = findViewById(R.id.tvACVaccineType);
+        VaccineTypeAdapter = new ArrayAdapter<>(this, R.layout.list_if_vaccined, VaccineType);
+        ifVaccinedAdapter = new ArrayAdapter<>(this, R.layout.list_if_vaccined, ifVaccined);
+        ACVaccined.setAdapter(ifVaccinedAdapter);
+        ACVaccineType.setAdapter(VaccineTypeAdapter);
+
+        ACVaccined.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String vaccined = parent.getItemAtPosition(position).toString();
+                if(vaccined == "Yes"){
+                    tlVaccinetype.setVisibility(view.VISIBLE);
+                    tl1stDose.setVisibility(view.VISIBLE);
+                    tl2ndDose.setVisibility(view.VISIBLE);
+                }
+                else if (vaccined == "No"){
+                    tlVaccinetype.setVisibility(view.GONE);
+                    tl1stDose.setVisibility(view.GONE);
+                    tl2ndDose.setVisibility(view.GONE);
+                }
+            }
+        });
+
+        ACVaccineType.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String vaccineType = parent.getItemAtPosition(position).toString();
+            }
+        });
+
+        MaterialDatePicker.Builder builder = MaterialDatePicker.Builder.datePicker();
+        builder.setTitleText("Select date");
+        final MaterialDatePicker materialDatePicker = builder.build();
+
+        MaterialDatePicker.Builder builder2 = MaterialDatePicker.Builder.datePicker();
+        builder2.setTitleText("Select date");
+        final MaterialDatePicker materialDatePicker2 = builder.build();
+
+        materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+            @Override
+            public void onPositiveButtonClick(Object selection) {
+                etFirstVaccine.setText(materialDatePicker.getHeaderText());
+            }
+        });
+
+        materialDatePicker2.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+            @Override
+            public void onPositiveButtonClick(Object selection) {
+                etSecondVaccine.setText(materialDatePicker.getHeaderText());
+            }
+        });
+
+        etFirstVaccine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                materialDatePicker.show(getSupportFragmentManager(), "DATE_PICKER");
+            }
+        });
+
+        etSecondVaccine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                materialDatePicker2.show(getSupportFragmentManager(), "DATE_PICKER");
+            }
+        });
 
         btnBack2.setOnClickListener(new View.OnClickListener() {
             @Override
